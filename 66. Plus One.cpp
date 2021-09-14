607c0ec94cf65d8d0a9936b2473d38d5a404d242
Amatulla Bohara <85250069+Amatulla-Bohara@users.noreply.github.com>
2021-09-14 18:24:44 +0530

    66. plus one leetcode problem

---
+++ 66. Plus One.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> ans;
+        int k=1;
+       int i=digits.size()-1;
+       while(i>=0 || k>0){
+           
+           if(i>=0){
+            ans.push_back((digits[i]+k)%10);
+            k=(digits[i]+k)/10;
+           }
+           
+           else{
+                ans.push_back(k%10);
+            k=k/10;
+           }
+           i--;
+        }
+
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
