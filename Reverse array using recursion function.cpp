5d5e87e10f5f82680bd8dae43b2bf407a9d853d9
Amatulla Bohara <85250069+Amatulla-Bohara@users.noreply.github.com>
2022-01-07 18:34:21 +0530

    Update Reverse array using recursion function.cpp

--- Reverse array using recursion function.cpp
+++ Reverse array using recursion function.cpp
@@ -5,7 +5,7 @@ void reverce_array(int arr[], int n){
     int left = 0;
     int right = n-1;
     
-    while(right>left){
+    while(right>left){  //by using while loop
         swap(arr[left++], arr[right--]);
     }
 }
@@ -26,8 +26,40 @@ int main() {
 	}
 	return 0;
 }
+---------------------------------------------------------------------------------------------------------------------------------------
+
+	
+	#include <iostream>
+using namespace std;
+
+void reverce_array(int arr[], int left, int right){
+   
+   if(right<=left){
+        return;
+    }
+    swap(arr[right], arr[left]);
+    reverce_array(arr, left+1, right-1);  //this is by using recursion
+}
 
 
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+	for (int i=0; i<n; i++){
+	    cin>>arr[i];
+	}
+
+	reverce_array( arr, 0 ,  n-1);
+	for(int i=0; i<n; i++){
+	    cout<<arr[i]<<" ";
+	}
+	return 0;
+}
+
+
+--------------------------------------------------------------------------------------------------------------------------------------
 
 /*
 input - 
