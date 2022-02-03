5dc93ee5257636606193a0ba3f2f7291e4f33f99
Amatulla Bohara <85250069+Amatulla-Bohara@users.noreply.github.com>
2022-02-03 22:38:06 +0530

    Create 454. 4Sum II.cpp

---
+++ 454. 4Sum II.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        
+        unordered_map <int, int> um;
+        
+        for(int n3: nums3){
+            for(int n4: nums4){
+                um[n4+n3]++;
+            }
+        }
+        int count = 0;
+        for(int n1: nums1){
+            for(int n2: nums2){
+                count = count + um[0-(n1+n2)];
+            }
+        }
+        return count;
+    }
+};
+
+/*
+Example 1:
+
+Input: nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+Output: 2
+Explanation:
+The two tuples are:
+1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+Example 2:
+
+Input: nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+Output: 1
+*/
