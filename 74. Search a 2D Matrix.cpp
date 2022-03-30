2c2d9d41110d1d272b9c37e4b0943b7b9a7ede3d
Amatulla Bohara <85250069+Amatulla-Bohara@users.noreply.github.com>
2022-03-30 23:52:56 +0530

    Create 74. Search a 2D Matrix.cpp

---
+++ 74. Search a 2D Matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(target==matrix[i][j]){
+                    return true;
+                }
+            }
+        }
+       return false;
+        
+    }
+};
+
+/*
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+  
+Example 2:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+*/
