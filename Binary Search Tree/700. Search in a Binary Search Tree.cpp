97b6192016a906ec35e07a0b0de9a32e4a308b23
Amatulla Bohara <85250069+Amatulla-Bohara@users.noreply.github.com>
2022-04-14 23:22:54 +0530

    Create 700. Search in a Binary Search Tree.cpp

---
+++ Binary Search Tree/700. Search in a Binary Search Tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL){
+            return root;
+        }
+        else if(root->val > val){
+            return searchBST(root->left, val);
+        }
+        else if(root->val < val){
+           return searchBST(root->right, val);
+        }
+        else{
+            return root;
+        }
+    }
+};
+
+
+
+/*
+Example 1:
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+Example 2:
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+*/
