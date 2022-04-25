1c20b39ea164dc0f6e51a33e54ccfa3924182525
Amatulla Bohara <85250069+Amatulla-Bohara@users.noreply.github.com>
2022-04-25 20:20:33 +0530

    Create 284. Peeking Iterator(2).cpp

---
+++ Stack/284. Peeking Iterator(2).cpp
@@ -0,0 +1,29 @@
+class PeekingIterator : public Iterator {
+public:
+    vector<int> v;
+    int i=0;
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    v=nums;
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        return v[i];
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	    return v[i++];
+	}
+	
+	bool hasNext() const {
+	    if(i<v.size()){
+            return true;
+        }
+        return false;
+	}
+};
