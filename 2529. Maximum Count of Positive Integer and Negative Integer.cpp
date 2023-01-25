e77d40564e8f283ca7947be880e8b00e7a53661f
Amatulla Bohara <85250069+Amatulla-Bohara@users.noreply.github.com>
2023-01-25 16:20:35 +0530

    Create 2529. Maximum Count of Positive Integer and Negative Integer.cpp

---
+++ 2529. Maximum Count of Positive Integer and Negative Integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        
+        int pos=0;
+        int neg=0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]>0){
+                pos++;
+            }
+            else if(nums[i]<0){
+                neg++;
+            }
+        }
+        int maxi = max(pos, neg);
+        return maxi;
+    }
+};
